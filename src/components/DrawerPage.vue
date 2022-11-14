af514aeac148fb95e6c9d34326bd35adbe646174
Wonderboy Mthiyane <codelad1@gmail.com>
2022-11-14 22:16:09 +0200

    added responsive css with device media queries
    Signed-off-by: Wonderboy Mthiyane <codelad1@gmail.com>

--- src/components/DrawerPage.vue
+++ src/components/DrawerPage.vue
@@ -4,41 +4,46 @@
     <div class="drawer-content flex flex-col">
       <!-- Navbar -->
       <div class="w-full navbar bg-base-300">
-        <div class="flex-none lg:hidden">
-          <label for="my-drawer-3" class="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              class="inline-block w-6 h-6 stroke-current"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </label>
-        </div>
-        <div class="flex-1 px-2 mx-2">Tech Box</div>
-        <div class="flex-none hidden lg:block">
-          <ul class="menu menu-horizontal">
-            <!-- Navbar menu content here -->
-            <li><a>Navbar Item 1</a></li>
-            <li><a>Navbar Item 2</a></li>
-          </ul>
+        <div class="wrapper">
+          <div class="flex-none lg:hidden">
+            <label for="my-drawer-3" class="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="inline-block w-6 h-6 stroke-current"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div class="flex-1 text-lg font-bold">Tech Box</div>
+          <div class="flex-none hidden lg:block">
+            <ul class="menu menu-horizontal">
+              <!-- Navbar menu content here -->
+              <li><a>Box 1</a></li>
+              <li><a>Box 2</a></li>
+            </ul>
+          </div>
         </div>
       </div>
-      <!-- Page content here -->
-      Content
+      <div class="wrapper">
+        <div class="content">
+          <h1>Content</h1>
+        </div>
+      </div>
     </div>
     <div class="drawer-side">
       <label for="my-drawer-3" class="drawer-overlay"></label>
       <ul class="menu p-4 w-80 bg-base-100">
         <!-- Sidebar content here -->
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
+        <li><a>Box 1</a></li>
+        <li><a>Box 2</a></li>
       </ul>
     </div>
   </div>
